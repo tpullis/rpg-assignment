df0e4dd4879bb59551744c7f1daeeee671865db7
Travis Pullis <pullist@traviss-mbp.lan>
2026-06-08 20:13:43 -0400

    chore: cleanup

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import UserPostsView from '../views/UserPostsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/users/:id',
       name: 'user-posts',
-      component: () => import('../views/UserPostsView.vue'),
+      component: UserPostsView,
     },
   ],
 })
